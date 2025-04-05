c935774b27dec08fc1cd24e1b72333ee065f63d7
aunshpatel <patelaunsh28@gmail.com>
2025-04-05 01:11:49 -0400

    Update files

--- server.js
+++ server.js
@@ -3,57 +3,23 @@ import path from "path";
 import mongoose from 'mongoose';
 import logger from "morgan";
 import dotenv from 'dotenv';
-
-// dotenv.config();
-
-// const app = express();
-
-// app.use(logger('dev'));
-// app.use(express.json());
-
-// mongoose.connect(process.env.MongoDB).then(()=>{
-//   console.log('Connected to MongoDB');
-// }).catch((err)=>{
-//   console.log(err);
-// });
-
-// app.listen(process.env.PORT || 3000, ()=>{
-//   console.log('Server is running on port 3000!');
-// });
-
-// app.use('/api/user', userRoutes);
-// app.use('/api/auth', authRoutes);
-// app.use('/api/todos', todoRoutes);
-
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
-// app.use(express.static(path.join(__dirname, 'build')));
-
-// app.get('*', function(req, res) {
-//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
-// });
-
+import { fileURLToPath } from 'url';
 
 dotenv.config();
 
 const app = express();
-
-app.use(logger("dev"));
 app.use(express.json());
 
-mongoose
-  .connect(process.env.MongoDB)
-  .then(() => {
-    console.log("Connected to MongoDB");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+// Connect to MongoDB
+mongoose.connect(process.env.MongoDB).then(() => console.log('MongoDB connected'))
+.catch(err => {
+  console.error('MongoDB connection error:', err);
+  process.exit(1);
+});
 
-app.use("/api/user", userRoutes);
-app.use("/api/auth", authRoutes);
-app.use("/api/todos", todoRoutes);
-app.use("/api", appVersionRoutes);
+app.get('/api', (req, res) => {
+  res.json({ message: 'Backend working!' });
+});
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    // tailwindcss(),
+  ],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        secure: false,
+      },
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      css: {
+        additionalData: `@import "./src/styles/global.css";`, // Ensure Tailwind has access
+      },
+    },
+  },
+});
