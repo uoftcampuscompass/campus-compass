b1409cf65db9e1a236ada9d00be7b6e13ff789b9
aunshpatel <patelaunsh28@gmail.com>
2025-04-09 01:19:01 -0400

    Add firebase reference

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB4W_po3QL4E2zMjB0T8ZSUKxAALT91uzE",
+  authDomain: "campus-compass-cfe72.firebaseapp.com",
+  projectId: "campus-compass-cfe72",
+  storageBucket: "campus-compass-cfe72.firebasestorage.app",
+  messagingSenderId: "751968527316",
+  appId: "1:751968527316:web:c0e8ab58f0b90bf0c16c41"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
